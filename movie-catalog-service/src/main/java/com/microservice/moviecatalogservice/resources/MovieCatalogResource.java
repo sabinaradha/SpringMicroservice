43c176f47dfe035cd0341378854bdb195b08c088
Sabina Koirala <sbnkrl@gmail.com>
2019-02-10 09:43:29 -0600

    Microservice project created

---
+++ movie-catalog-service/src/main/java/com/microservice/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,25 @@
+package com.microservice.moviecatalogservice.resources;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservice.moviecatalogservice.models.CatalogItem;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+	
+	@RequestMapping("/{userId}")
+	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+		
+		return Collections.singletonList(
+				new CatalogItem("Transformer", "Test", 4)
+				);
+		
+	}
+
+}
